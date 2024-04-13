962617bef666cf171aa8d43b0a4bdba65d700e8d
Radomir <rad.tretyakov@yandex.kz>
2024-04-13 11:46:56 +0500

    Fixes

--- models.py
+++ models.py
@@ -13,6 +13,7 @@ POSTGRS_PORT = os.getenv("POSTGRES_PORT", "5431")
 
 PG_DSN = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRS_PORT}/{POSTGRES_DB}"
 
+
 engine = create_async_engine(PG_DSN)
 Session = async_sessionmaker(engine, expire_on_commit=True)
 
